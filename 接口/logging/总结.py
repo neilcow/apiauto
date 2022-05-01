c8732266a5c89685ae3a96047dc6084167233a45
niujinliang <2415793@qq.com>
2022-05-01 14:21:18 +0800

    day09

---
+++ 接口/logging/总结.py
@@ -0,0 +1,12 @@
+
+import logging
+
+a = 1
+b = 2
+c = a + b
+# 记录debug信息
+logging.info('这是一个普通信息')
+logging.debug('这是一个debug信息')
+logging.warning('这是一个警告信息')
+logging.error('出错了')
+logging.critical('崩溃了')
